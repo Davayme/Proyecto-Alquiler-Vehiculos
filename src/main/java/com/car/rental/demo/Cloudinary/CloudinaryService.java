adb2e16701de604ba6870c6cc4da89f94e95b9de
Henry Cortez <henrystevencortez@yahoo.es>
2024-11-12 12:25:52 -0500

    se hizo que cambie todo a un .env

--- src/main/java/com/car/rental/demo/Cloudinary/CloudinaryService.java
+++ src/main/java/com/car/rental/demo/Cloudinary/CloudinaryService.java
@@ -8,23 +8,25 @@ import org.springframework.web.multipart.MultipartFile;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @Service
 public class CloudinaryService {
     private final Cloudinary cloudinary;
 
     public CloudinaryService() {
+        Dotenv dotenv = Dotenv.load();
         cloudinary = new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", "dyawf0vyu",
-            "api_key", "616421482787377",
-            "api_secret", "-KrOSBUStjj0Aav1y4Nalhg3rGo"));
+                "cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"),
+                "api_key", dotenv.get("CLOUDINARY_API_KEY"),
+                "api_secret", dotenv.get("CLOUDINARY_API_SECRET")));
     }
 
     public String uploadImage(MultipartFile file) throws IOException {
-        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), 
-            ObjectUtils.asMap(
-                "public_id", file.getOriginalFilename(),
-                "format", "jpg"
-            ));
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(),
+                ObjectUtils.asMap(
+                        "public_id", file.getOriginalFilename(),
+                        "format", "jpg"));
         return uploadResult.get("url").toString();
     }
 }
