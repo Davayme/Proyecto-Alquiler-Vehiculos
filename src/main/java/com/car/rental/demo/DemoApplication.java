ac377f5986b5bd58676f6778c306c99c71d50a14
Henry Cortez <henrystevencortez@yahoo.es>
2024-11-13 14:51:00 -0500

    prueba 2

--- src/main/java/com/car/rental/demo/DemoApplication.java
+++ src/main/java/com/car/rental/demo/DemoApplication.java
@@ -9,7 +9,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
+		Dotenv dotenv = Dotenv.configure()
+		.directory("/etc/secrets") // Ruta del directorio
+		.filename(".env")          // Nombre del archivo
+		.load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
        
 		SpringApplication.run(DemoApplication.class, args);
