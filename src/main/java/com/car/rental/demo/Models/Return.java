d568390b1567b9bc55a8af8b62d173a9842c5d34
Henry Cortez <henrystevencortez@yahoo.es>
2024-12-15 09:56:06 -0500

    Add JavaFaker dependency and refactor Vehicle, Rental, and Return models to use Lombok; implement automatic generation of chassis and engine numbers

--- src/main/java/com/car/rental/demo/Models/Return.java
+++ src/main/java/com/car/rental/demo/Models/Return.java
@@ -1,8 +1,16 @@
 package com.car.rental.demo.Models;
 
 import jakarta.persistence.*;
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "returns")
 public class Return {
@@ -21,43 +29,5 @@ public class Return {
     private String vehicleCondition;
     private double extraCharges;
 
-    public Long getReturnId() {
-        return returnId;
-    }
 
-    public void setReturnId(Long returnId) {
-        this.returnId = returnId;
-    }
-
-    public Rental getRental() {
-        return rental;
-    }
-
-    public void setRental(Rental rental) {
-        this.rental = rental;
-    }
-
-    public Date getReturnDate() {
-        return returnDate;
-    }
-
-    public void setReturnDate(Date returnDate) {
-        this.returnDate = returnDate;
-    }
-
-    public String getVehicleCondition() {
-        return vehicleCondition;
-    }
-
-    public void setVehicleCondition(String vehicleCondition) {
-        this.vehicleCondition = vehicleCondition;
-    }
-
-    public double getExtraCharges() {
-        return extraCharges;
-    }
-
-    public void setExtraCharges(double extraCharges) {
-        this.extraCharges = extraCharges;
-    }
 }
--- src/main/java/com/car/rental/demo/Models/Vehicle.java
+++ src/main/java/com/car/rental/demo/Models/Vehicle.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.github.javafaker.Faker;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -20,7 +21,8 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -34,7 +36,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "vehicles")
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "vehicleId")
 public class Vehicle {
-
+  private static final Faker faker = new Faker();
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long vehicleId;
@@ -43,6 +45,15 @@ public class Vehicle {
   private String model;
   @Column(unique = true, nullable = false)
   private String licensePlate;
+
+  //@Column(unique = true, nullable = false)
+  //@NotBlank(message = "Chasis number cannot be blank")
+  @Pattern(regexp = "^[A-HJ-NPR-Z0-9]{17}$", message = "Invalid chasis number format")
+  private String chasisNumber;
+  //@Column(unique = true, nullable = false)
+  //@NotBlank(message = "Chasis number cannot be blank")
+  @Pattern(regexp = "^[A-Z0-9]{8,12}$", message = "Invalid engine number format")
+  private String engineNumber;
 
   @Enumerated(EnumType.STRING)
   @Column(nullable = false)
@@ -87,5 +98,20 @@ public class Vehicle {
   public enum TransmissionType {
     MANUAL, 
     AUTOMATIC 
+  }
+
+  private String generateChasisNumber() {
+      return faker.regexify("[A-HJ-NPR-Z0-9]{17}");
+  }
+
+  private String generateEngineNumber() {
+      return faker.regexify("[A-Z0-9]{8,12}");
+  }
+
+  public void setAutoChasis(){
+    this.chasisNumber = generateChasisNumber();
+  }
+  public void setAutoEngine(){
+    this.engineNumber = generateEngineNumber();
   }
 }
